dd0d8fa247989b3cc88f1a47e53a6e8d8c089084
Farzana-taj <84780163+Farzana-taj@users.noreply.github.com>
2021-05-26 16:54:30 +0530

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,17 @@
+function c2F(celsius) 
+{
+  const cTemp = celsius;
+  const cToFahr = cTemp * 9 / 5 + 32;
+  const message = `${cTemp}\xB0C is ${cToFahr} \xB0F.`;
+    console.log(message);
+}
+
+function f2C(fahrenheit) 
+{
+  const fTemp = fahrenheit;
+  const fToCel = (fTemp - 32) * 5 / 9;
+  const message = `${fTemp}\xB0F is ${fToCel}\xB0C.`;
+    console.log(message);
+} 
+c2F(105);
+f2C(50);
